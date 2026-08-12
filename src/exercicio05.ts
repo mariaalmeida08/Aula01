1acfe55cc83af36d5303b44ff6527cab1eb3c92f
mariaalmeida08 <dudinha080430@gmail.com>
2026-08-12 11:32:55 -0300

    Add files via upload

---
+++ src/exercicio05.ts
@@ -0,0 +1,23 @@
+import { createInterface } from "node:readline/promises";
+
+async function main(): Promise<void> {
+    const leitor = createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    const limite: number = Number (
+        await leitor.question ("Digite o limite da contagem: ")
+    );
+
+    let contador: number = 1;
+
+    while (contador <= limite) {
+        console.log(`contador: ${contador}`);
+        contador++;
+    }
+
+    leitor.close();
+}
+
+main();
